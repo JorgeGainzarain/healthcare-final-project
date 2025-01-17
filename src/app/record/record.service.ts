437c0f4a18358a1667acf6e3e5cf3ed787cfc5ca
neodr <neodragon1928@gmail.com>
2025-01-17 05:06:41 +0100

    Record tests finished + functionality fixed

--- src/app/record/record.service.ts
+++ src/app/record/record.service.ts
@@ -13,6 +13,8 @@ import { validateUpdate } from './validations/validateUpdate';
 import {UserType} from "../user/user.model";
 import {StatusError} from "../../utils/status_error";
 import {NotificationService} from "../notification/notification.service";
+import {PatientRepository} from "../patient/patient.repository";
+import {DoctorRepository} from "../doctor/doctor.repository";
 
 @Service()
 export class RecordService extends  BaseService<Record> {
@@ -22,19 +24,21 @@ export class RecordService extends  BaseService<Record> {
         protected recordRepository: RecordRepository,
         protected auditService: LogService,
 
-        protected notificationService: NotificationService
+        protected notificationService: NotificationService,
+        protected patientRepository: PatientRepository,
+        protected doctorRepository: DoctorRepository
     ) {
         super(recordRepository, auditService);
         this.notificationService= notificationService;
+        this.patientRepository = patientRepository;
+        this.doctorRepository = doctorRepository;
     }
 
     async findAll(session: Session & Partial<SessionData>): Promise<Record[]> {
+        await this.before(ActionType.VIEW_ALL, [session]);
         const user_id = session.userId;
         let records = await this.recordRepository.findAll();
         const role = session.role;
-        if (!role) {
-            throw new StatusError(403, 'You must be logged in to perform this action');
-        }
         if (role !== UserType.ADMIN) {
             records = records.filter(record => {
                 if (role === UserType.DOCTOR) {
@@ -45,6 +49,7 @@ export class RecordService extends  BaseService<Record> {
                 }
             });
         }
+        await this.after(ActionType.VIEW_ALL, records, [session]);
         await this.logAction(user_id!, records, 'retrieved');
         return records;
     }
@@ -64,9 +69,13 @@ export class RecordService extends  BaseService<Record> {
                 await validateView(role, args);
                 break;
             case ActionType.CREATE:
+                args[2] = this.patientRepository;
+                args[3] = this.doctorRepository;
                 await validateCreate(role, args);
                 break;
             case ActionType.UPDATE:
+                const record = await this.recordRepository.findById(args[1]);
+                args[1] = record;
                 await validateUpdate(role, args);
                 break;
         }
@@ -75,9 +84,6 @@ export class RecordService extends  BaseService<Record> {
     async after(action: ActionType, result: any, args: any[]): Promise<any> {
         const session = args[0] as Session & Partial<SessionData>;
         const role = session.role;
-        if (!role) {
-            throw new StatusError(403, 'You must be logged in to perform this action');
-        }
         if (role === UserType.ADMIN) {
             return result;
         }
